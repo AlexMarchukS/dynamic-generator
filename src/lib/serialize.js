822359b2714f6a562407edfa8a0d5b879601e501
Illia Osmanov <illiaosmanov@gmail.com>
2020-02-21 01:57:57 +0200

    refactored serialize

--- src/lib/serialize.js
+++ src/lib/serialize.js
@@ -1,9 +1,18 @@
-/* eslint-disable */
 import * as vv from 'vuelidate/lib/validators';
 
-const excludeMeta = (payload) => {
+function excludeMeta(payload) {
+  // eslint-disable-next-line
   const { value, type, label, ...rest } = payload;
   return rest;
+}
+
+function toVuelidateSchema(field) {
+  return Object.keys(field).reduce((acc, rule) => ({
+    ...acc,
+    [rule]: field[rule].params
+      ? vv[rule].call(null, field[rule].params)
+      : vv[rule].bind(null)
+  }), {})
 }
 
 function serialize(payload) {
@@ -13,26 +22,18 @@ function serialize(payload) {
   function validationSchema(schema) {
     for (const key in schema) {
       const prop = schema[key];
-
       if (prop) {
-        const isSpecified = prop.hasOwnProperty(searchableField);
+        const isSpecified = Object.prototype.hasOwnProperty.call(prop, searchableField);
+
         if (typeof prop === 'object') {
           validationSchema(prop)
         }
 
         if (isSpecified) {
-          let body = {};
-          const field = prop[searchableField];
-
-          for (const rule in field) {
-            body = {
-              ...body,
-              [rule]: field[rule].params
-                ? vv[rule].call(null, field[rule].params)
-                : vv[rule].bind(null)
-            }
+          schema[key] = {
+            ...excludeMeta(prop),
+            ...toVuelidateSchema(prop[searchableField])
           }
-          schema[key] = { ...excludeMeta(prop), ...body }
           delete schema[key][searchableField]
         }
       }
